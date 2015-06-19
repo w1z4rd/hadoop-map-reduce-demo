0a4562a81c53033a2b0ede5b0d13d3683dfd8eeb
Costel Radulescu <costel.radulescu.cs@gmail.com>
2015-06-19 17:52:45 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#hadoop-map-reduce-job:
+url: http://hadoop.apache.org/docs/r1.2.1/mapred_tutorial.html
---
+++ src/main/java/org/apache/hadoop/demo/Reduce.java
@@ -0,0 +1,28 @@
+package org.apache.hadoop.demo;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class Reduce extends MapReduceBase implements
+		Reducer<Text, IntWritable, Text, IntWritable> {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void reduce(Text arg0, Iterator<IntWritable> arg1,
+			OutputCollector<Text, IntWritable> arg2, Reporter arg3)
+			throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
