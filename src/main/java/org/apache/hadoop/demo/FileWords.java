637ad717294b8b48b56dabe4179e5f09b96e006c
Costel Radulescu <costel.radulescu.cs@gmail.com>
2015-08-05 14:04:42 +0300

    added FindMissingPlates and FileWords

--- src/main/java/org/apache/hadoop/demo/FileWords.java
+++ src/main/java/org/apache/hadoop/demo/FileWords.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
+import org.apache.hadoop.demo.WordCount.Map;
+import org.apache.hadoop.demo.WordCount.Reduce;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MapReduceBase;
@@ -20,22 +23,45 @@ import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.TextOutputFormat;
 
-public class WordCount {
+public class FileWords {
 
   public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
-    private final static IntWritable one = new IntWritable(1);
+    private final static IntWritable ONE = new IntWritable(1);
+    private final static IntWritable TWO = new IntWritable(2);
+    private final static String FILE_A = "fileA";
+    private final static String FILE_B = "fileB";
+
     private Text word = new Text();
 
     public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
         throws IOException {
+      FileSplit fsplit = (FileSplit) reporter.getInputSplit();
+      String inputFileName = fsplit.getPath().getName();
       String line = value.toString();
       StringTokenizer tokenizer = new StringTokenizer(line);
       while (tokenizer.hasMoreTokens()) {
-        word.set(tokenizer.nextToken());
-        output.collect(word, one);
+        String token = tokenizer.nextToken();
+        word.set(token);
+        if (inputFileName.equalsIgnoreCase(FILE_A))
+          output.collect(word, ONE);
+        else if (inputFileName.equalsIgnoreCase(FILE_B))
+          output.collect(word, TWO);
       }
     }
+  }
+
+  public static class Combine extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
+        Reporter reporter) throws IOException {
+      int val = 0;
+      while (values.hasNext()) {
+        val = values.next().get();
+      }
+      output.collect(key, new IntWritable(val));
+    }
+
   }
 
   public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
@@ -52,13 +78,13 @@ public class WordCount {
 
   public static void main(String[] args) throws IOException {
     JobConf conf = new JobConf(WordCount.class);
-    conf.setJobName("demo-wordcount");
+    conf.setJobName("license-plates");
 
     conf.setOutputKeyClass(Text.class);
     conf.setOutputValueClass(IntWritable.class);
 
     conf.setMapperClass(Map.class);
-    conf.setCombinerClass(Reduce.class);
+    conf.setCombinerClass(Combine.class);
     conf.setReducerClass(Reduce.class);
 
     conf.setInputFormat(TextInputFormat.class);
--- src/main/java/org/apache/hadoop/demo/FindMissingPlates.java
+++ src/main/java/org/apache/hadoop/demo/FindMissingPlates.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
+import org.apache.hadoop.demo.WordCount.Map;
+import org.apache.hadoop.demo.WordCount.Reduce;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MapReduceBase;
@@ -20,21 +23,33 @@ import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.TextOutputFormat;
 
-public class WordCount {
+public class FindMissingPlates {
 
   public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
-    private final static IntWritable one = new IntWritable(1);
+    private final static IntWritable ONE = new IntWritable(1);
+    private final static IntWritable TWO = new IntWritable(2);
+    private final static IntWritable THREE = new IntWritable(3);
+    private final static String LICENSE_PLATES = "file01.txt";
+    private final static String LICENSE_PLATES_PIPELINE = "file02.txt";
+    private final static String LICENSE_PLATES_STREAM = "file03.txt";
+
     private Text word = new Text();
 
     public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
         throws IOException {
+      FileSplit fsplit = (FileSplit) reporter.getInputSplit();
+      String inputFileName = fsplit.getPath().getName();
       String line = value.toString();
       StringTokenizer tokenizer = new StringTokenizer(line);
-      while (tokenizer.hasMoreTokens()) {
-        word.set(tokenizer.nextToken());
-        output.collect(word, one);
-      }
+      String token = tokenizer.nextToken();
+      word.set(token);
+      if (inputFileName.equalsIgnoreCase(LICENSE_PLATES))
+        output.collect(word, ONE);
+      else if (inputFileName.equalsIgnoreCase(LICENSE_PLATES_PIPELINE))
+        output.collect(word, TWO);
+      else if (inputFileName.equalsIgnoreCase(LICENSE_PLATES_STREAM))
+        output.collect(word, THREE);
     }
   }
 
@@ -52,7 +67,7 @@ public class WordCount {
 
   public static void main(String[] args) throws IOException {
     JobConf conf = new JobConf(WordCount.class);
-    conf.setJobName("demo-wordcount");
+    conf.setJobName("license-plates");
 
     conf.setOutputKeyClass(Text.class);
     conf.setOutputValueClass(IntWritable.class);
